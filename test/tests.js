387e998090e1b5c537df2d40e80151c18030c3f4
Jorge Hernandez <molamil@gmail.com>
2011-12-06 18:06:08 +0100

    Updated unit tests

--- test/tests.js
+++ test/tests.js
@@ -16,9 +16,11 @@ var _f = function() {
 	_s += "a";
 };
 
-Agent.observe("_f", function() {
+var _fb = function() {
 	_s += "b";
-});
+}
+
+Agent.observe("_f", _fb);
 
 Agent.observe("_f", function() {
 	_s += "c";
@@ -28,55 +30,56 @@ _f();
 
 test("Agent on window scope", function() {
 
-		equal(_s, "abc", "s = 'abc'");
+	equal(_s, "abc", "s = 'abc'");
 
-	}
-);
+	_s = "";
+	Agent.ignore("_f", _fb);
+	_f();
+	equal(_s, "ac", "s = 'ac'");
+
+});
 
 test("Agent on function scope", function() {
 
-		var s = "";
+	var s = "";
+	var o = {};
+	o.f = function() {
+		s += "a";
+	};
 
-		var o = {};
+	Agent.observe(o, "f", function() {
+		s += "b";
+	}, 10);
 
-		o.f = function() {
-			s += "a";
-		};
+	Agent.observe(o, "f", function() {
+		s += "c";
+	}, 5);
 
-		Agent.observe(o, "f", function() {
-			s += "b";
-		}, 10);
+	Agent.observe(o, "f", function() {
+		s += "d";
+	}, 5);
 
-		Agent.observe(o, "f", function() {
-			s += "c";
-		}, 5);
+	Agent.observe(o, "f", function() {
+		s += "e";
+	});
 
-		Agent.observe(o, "f", function() {
-			s += "d";
-		}, 5);
+	Agent.observe(o, "f", function() {
+		s += "f";
+	});
 
-		Agent.observe(o, "f", function() {
-			s += "e";
-		});
+	var fz = function() {
+		s += "z";
+	};
 
-		Agent.observe(o, "f", function() {
-			s += "f";
-		});
+	Agent.observe(o, "f", fz);
 
-		var fz = function() {
-			s += "z";
-		};
-	
-		Agent.observe(o, "f", fz);
+	Agent.ignore(o, "f", fz);
 
-		Agent.ignore(o, "f", fz);
+	o.f();
 
-		o.f();
+	equal(s, "abcdef", "s = 'abcdef'");
 
-		equal(s, "abcdef", "s = 'abcdef'");
-
-	}
-);
+});
 
 
 // *** SCOPED
@@ -85,23 +88,138 @@ module("Scoped");
 
 test("Priorities", function() {
 
-		var s = "";
+	var s = "";
+	var o = {};
+	o.f = function() {
+		s += "a";
+	};
 
-		var f = function() {
-			s += "a";
-		};
+	Agent.observe(o, "f", function() {
+		s += "d";
+	}, 1);
 
-		Agent.observe(this, "f", function() {
-			s += "b";
-		}, 1);
+	Agent.observe(o, "f", function() {
+		s += "e";
+	});
 
-		Agent.observe(this, "f", function() {
-			s += "c";
-		}, 1);
+	Agent.observe(o, "f", function() {
+		s += "b";
+	}, 5);
 
-		f();
-	
-		equal(s, "abc", "s = 'abc'");
+	Agent.observe(o, "f", function() {
+		s += "c";
+	}, 5);
 
-	}
-);
+	o.f();
+
+	equal(s, "abcde", "s = 'abcde'");
+
+});
+
+test("Ignore", function() {
+
+	var s = "";
+	var o = {};
+	o.f = function() {
+		s += "a";
+	};
+
+	var ob = function() {
+		s += "b";
+	};
+
+	var oc = function() {
+		s += "c";
+	};
+
+	Agent.observe(o, "f", ob);
+	Agent.observe(o, "f", oc);
+
+	Agent.ignore(o, "f", oc);
+	Agent.ignore(o, "f", ob);
+
+	o.f();
+
+	equal(s, "a", "s = 'a'");
+
+});
+
+test("This context", function() {
+
+	var o = {
+
+		s: "",
+		dot: ".",
+
+		f: function() {
+			this.s = "a";
+		},
+
+		addDot: function() {
+			this.s += this.dot;
+		}
+
+	};
+
+	var oo = {
+
+		addDash: function() {
+			this.s += "-";
+		}
+
+	};
+
+	equal(o.s, "", "s = ''");
+
+	o.f();
+	equal(o.s, "a", "s = 'a'");
+
+	Agent.observe(o, "f", o.addDot);
+	o.f();
+	equal(o.s, "a.", "s = 'a.'");
+
+	Agent.observe(o, "f", oo.addDash, oo);
+	o.f();
+	equal(o.s, "a.", "s = 'a.'");
+
+	Agent.ignore(o, "f", oo.addDash);
+	Agent.observe(o, "f", oo.addDash, o);
+	o.f();
+	equal(o.s, "a.-", "s = 'a.-'");
+
+	Agent.observe(o, "f", function() {
+		this.s += "z";
+	});
+	o.f();
+	equal(o.s, "a.-z", "s = 'a.-z'");
+
+	var n = 0;
+	var ooo = {
+		n: 10
+	};
+	Agent.observe(o, "f", function() {
+		n++;
+	});
+	o.f();
+	equal(n, 1, "n = 1");
+	equal(ooo.n, 10, "ooo.n = 10");
+	equal(o.s, "a.-z", "s = 'a.-z'");
+
+	Agent.observe(o, "f", function() {
+		this.n++;
+	});
+	o.f();
+	equal(n, 2, "n = 2");
+	equal(ooo.n, 10, "ooo.n = 10");
+	equal(o.s, "a.-z", "s = 'a.-z'");
+
+
+	Agent.observe(o, "f", function() {
+		this.n++;
+	}, ooo);
+	o.f();
+	equal(n, 3, "n = 3");
+	equal(ooo.n, 11, "ooo.n = 11");
+	equal(o.s, "a.-z", "s = 'a.-z'");
+
+});
